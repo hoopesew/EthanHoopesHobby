7b21861a6720814c86f5287d4baba48eedfc8c72
ethanhoopes <hoopesew@gmail.com>
2024-02-09 18:24:22 -0800

    Add project files.

---
+++ EthanHoopesHobby/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $("#calculationForm").submit(function (event) {
+        event.preventDefault(); // Prevent form from submitting normally
+        var hours = $("#hours").val();
+        var rate = $("#rate").val();
+        var total = hours * rate;
+        $("#result").text("Total amount: $" + total); // Display the result
+    });
+});
+
